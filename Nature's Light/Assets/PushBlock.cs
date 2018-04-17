c79e0d6006b040ef00ba278b473b38d50e52b4f1
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-04-17 15:44:42 -0700

    HUD and started movable blocks

---
+++ Nature's Light/Assets/PushBlock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBlock : MonoBehaviour {
+
+    public int playerSpeed;
+    private bool isPushed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isPushed)
+        {
+            if (Input.GetKey("up") || Input.GetKey("w"))
+            {
+                transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey("down") || Input.GetKey("s"))
+            {
+                transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey("left") || Input.GetKey("a"))
+            {
+                transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey("right") || Input.GetKey("d"))
+            {
+                transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isPushed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isPushed = false;
+        }
+    }
+}
