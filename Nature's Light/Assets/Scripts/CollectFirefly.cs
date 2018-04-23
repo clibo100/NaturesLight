c2910bc8c05874482c0d47b8e8b27c2bb86a1d1b
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-04-23 13:52:53 -0700

    score updates on screen, game controller working.

--- Nature's Light/Assets/Scripts/CollectFirefly.cs
+++ Nature's Light/Assets/Scripts/CollectFirefly.cs
@@ -6,10 +6,21 @@ public class CollectFirefly : MonoBehaviour {
     public float speed = 5f;
     public float height = 0.5f;
     public Light lt;
+    private int scoreValue = 1;
+    private GameController gameController;
 
     // Use this for initialization
     void Start () {
-	}
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,7 +35,9 @@ public class CollectFirefly : MonoBehaviour {
             {
                 lt.intensity += (float)0.3;
             }
+            gameController.AddScore(scoreValue);
             Destroy(gameObject);
         }
     }
+    
 }
--- Nature's Light/Assets/Scripts/GameController.cs
+++ Nature's Light/Assets/Scripts/GameController.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
+    private int score;
+    public Text scoreText;
+
 	// Use this for initialization
 	void Start () {
-		
+        score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "x " + score;
+    }
 }
